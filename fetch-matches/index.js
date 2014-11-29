1adf0bd4b814a31cb2eecbf994ff3e6201aeb296
Kenan Yildirim <kenan@kenany.me>
2014-11-29 12:37:18 -0500

    Fetch final scores

--- fetch-matches/index.js
+++ fetch-matches/index.js
@@ -3,6 +3,7 @@ var eachAsync = require('each-series');
 var getMatches = require('gosugamers-matches');
 var through = require('through');
 var inherits = require('inherits');
+var isNaN = require('lodash.isnan');
 
 function FetchMatchesStream(pages) {
   if (!(this instanceof FetchMatchesStream)) {
@@ -38,13 +39,24 @@ function FetchMatchesStream(pages) {
     });
 
     allMatches.forEach(function(match) {
-      writer.write({
+      var ret = {
         date: (new Date(match.date * 1000)).toISOString(),
         team1: match.opponent1,
         team2: match.opponent2,
         result1: match.score1,
-        result2: match.score2
-      });
+        result2: match.score2,
+        score1: 0,
+        score2: 0
+      };
+
+      for (var i = 1; i < 6; i++) {
+        if (match['round' + i] && !isNaN(match['round' + i].home)) {
+          ret.score1 += match['round' + i].home;
+          ret.score2 += match['round' + i].away;
+        }
+      }
+
+      writer.write(ret);
     });
 
     writer.end();
