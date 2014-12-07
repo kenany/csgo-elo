0f514f4a8fb5d9cb9de9b1d5569d1f890691a93d
Kenan Yildirim <kenan@kenany.me>
2014-12-06 23:20:44 -0500

    esea-s17-lan: update with current results

--- esea-s17-lan/index.js
+++ esea-s17-lan/index.js
@@ -26,14 +26,28 @@ function predict(standings) {
     results.push(res);
   });
 
-  results[4] = predictMatch(standings, results[1].loser, results[2].loser);
-  results[5] = predictMatch(standings, results[0].loser, results[3].loser);
+  results[0] = { winner: 'Titan.CS', loser: 'Denial eSports' };
+  results[1] = { winner: 'fnatic', loser: 'eLevate' };
+  results[2] = { winner: 'Virtus.Pro.CS', loser: 'iBUYPOWER' };
+  results[3] = { winner: 'Cloud9.CS', loser: 'mousesports.CS' };
 
-  results[6] = predictMatch(standings, results[1].winner, results[2].winner);
-  results[7] = predictMatch(standings, results[0].winner, results[3].winner);
+  // results[4] = predictMatch(standings, results[0].loser, results[1].loser);
+  // results[5] = predictMatch(standings, results[2].loser, results[3].loser);
 
-  results[8] = predictMatch(standings, results[4].winner, results[6].loser);
-  results[9] = predictMatch(standings, results[5].winner, results[7].loser);
+  results[4] = { winner: 'Denial eSports', loser: 'eLevate' };
+  results[5] = { winner: 'iBUYPOWER', loser: 'mousesports' };
+
+  // results[6] = predictMatch(standings, results[0].winner, results[1].winner);
+  // results[7] = predictMatch(standings, results[2].winner, results[3].winner);
+
+  results[6] = { winner: 'fnatic', loser: 'Titan.CS' };
+  results[7] = { winner: 'Virtus.Pro.CS', loser: 'Cloud9.CS'};
+
+  // results[8] = predictMatch(standings, results[4].winner, results[6].loser);
+  // results[9] = predictMatch(standings, results[5].winner, results[7].loser);
+
+  results[8] = { winner: 'Denial eSports', loser: 'Cloud9.CS' };
+  results[9] = { winner: 'iBUYPOWER', loser: 'Titan.CS' };
 
   results[10] = predictMatch(standings, results[8].winner, results[9].winner);
 
