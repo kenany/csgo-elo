10c16a7bff629591deabb58840de1b238f1b9e30
Kenan Yildirim <kenan@kenany.me>
2015-01-08 12:14:36 -0500

    glick accuracy check

--- glicko.js
+++ glicko.js
@@ -4,21 +4,31 @@ var pairs = require('lodash.pairs');
 var sortBy = require('lodash.sortby');
 var forEachRight = require('lodash.foreachright');
 
+var ALIASES = {
+  'Virtus Pro-CS': 'Virtus.Pro',
+  'Virtus.Pro.CS': 'Virtus.Pro',
+  'LunatiK eSports': 'LunatiK',
+  'compLexity Gaming': 'Cloud9',
+  'Cloud9.CS': 'Cloud9',
+  'Team Dignitas.CS': 'Team Dignitas'
+};
+
 var glicko = new Glicko();
 
 var teams = {};
+var matches = [];
+var correct = 0;
 
 process.stdin
   .pipe(csv())
   .on('data', function(row) {
-    row.team1 = row.team1.replace('Virtus Pro-CS', 'Virtus.Pro.CS');
-    row.team2 = row.team2.replace('Virtus Pro-CS', 'Virtus.Pro.CS');
+    if (ALIASES[row.team1]) {
+      row.team1 = ALIASES[row.team1];
+    }
 
-    row.team1 = row.team1.replace('LunatiK eSports', 'LunatiK');
-    row.team2 = row.team2.replace('LunatiK eSports', 'LunatiK');
-
-    row.team1 = row.team1.replace('compLexity Gaming', 'Cloud9.CS');
-    row.team2 = row.team2.replace('compLexity Gaming', 'Cloud9.CS');
+    if (ALIASES[row.team2]) {
+      row.team2 = ALIASES[row.team2];
+    }
 
     if (!teams[row.team1]) {
       teams[row.team1] = glicko.makePlayer();
@@ -30,10 +40,23 @@ process.stdin
     var result1 = parseInt(row.result1, 10);
     var result2 = parseInt(row.result2, 10);
 
+    var odds1 = teams[row.team1]._E(teams[row.team2].getRating(), teams[row.team2].getRd());
+    var odds2 = teams[row.team2]._E(teams[row.team1].getRating(), teams[row.team1].getRd());
+
+    if (result1 > result2 && odds1 > odds2) {
+      correct++;
+    }
+    if (result2 > result1 && odds2 > odds1) {
+      correct++;
+    }
+
+    matches.push([teams[row.team1], teams[row.team2], +(result1 > result2)]);
+
     glicko.updateRatings([[teams[row.team1], teams[row.team2], +(result1 > result2)]]);
   })
   .on('end', function() {
     console.log();
+
     var rankings = sortBy(pairs(teams), function(team) {
       return team[1].getRating();
     });
@@ -41,4 +64,6 @@ process.stdin
     forEachRight(rankings, function(team, i) {
       console.log('%d. %s (%d)', rankings.length - i, team[0], team[1].getRating());
     });
+
+    console.log(100 * correct / matches.length);
   });
