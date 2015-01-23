c759611e94a8f85cb82a38fc992f9c187c618224
Kenan Yildirim <kenan@kenany.me>
2015-01-23 07:23:42 -0500

    what the heck is "LunatiK: B"

--- aliases/index.js
+++ aliases/index.js
@@ -2,6 +2,7 @@ var ALIASES = {
   'Virtus Pro-CS': 'Virtus.Pro',
   'Virtus.Pro.CS': 'Virtus.Pro',
   'LunatiK eSports': 'LunatiK',
+  'LunatiK: B': 'LunatiK',
   'Cloud9.CS': 'Cloud9',
   'Team Dignitas.CS': 'Team Dignitas'
 };
