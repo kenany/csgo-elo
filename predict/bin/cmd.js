9370cfa29f1ce1b2ea5b77c6e2c365d5a5486dec
Kenan Yildirim <kenan@kenany.me>
2015-03-05 10:10:10 -0500

    new predict

--- predict/bin/cmd.js
+++ predict/bin/cmd.js
@@ -4,6 +4,7 @@ var csv = require('csv-parser');
 var minimist = require('minimist');
 var printf = require('printf');
 var table = require('text-table');
+var dup = require('dup');
 
 var predict = require('../')
 
@@ -17,11 +18,19 @@ process.stdin
     matches.push(row);
   })
   .on('end', function() {
-    var infer = predict(argv._[0], argv._[1], matches);
+    var n = (argv._[2] + 1) / 2;
+    var infer = predict(matches, argv._[0], argv._[1], n);
+
+    var outcomes = dup([n + 1, n + 1]);
+    infer.outcomes.forEach(function(o) {
+      outcomes[o[1]][o[2]] = o[0];
+    });
+
     var t = table([
       ['', argv._[0], argv._[1]],
       ['Elo', printf('%.2f\%', infer.elo * 100), printf('%.2f\%', 100 - infer.elo * 100)],
       ['Glicko', printf('%.2f\%', infer.glicko * 100), printf('%.2f\%', 100 - infer.glicko * 100)],
     ]);
     console.log(t);
+    console.log('Median outcome is %s-%s', infer.median[1], infer.median[2]);
   });
--- predict/index.js
+++ predict/index.js
@@ -1,16 +1,71 @@
 var Elo = require('arpad');
+var map = require('lodash.map');
+var sortBy = require('lodash.sortby');
+var binomial = require('choose');
+var sum = require('compute-sum');
+
 var eloCalculate = require('@csgo-elo/elo');
 var glickoCalculate = require('@csgo-elo/glicko');
 
-function predict(team1, team2, matches) {
+function predict(matches, team1, team2, n) {
   var eloRatings = eloCalculate(matches);
   var glickoRatings = glickoCalculate(matches);
 
   var elo = new Elo();
 
+  var epa = elo.expectedScore(eloRatings[team1], eloRatings[team2]);
+  var epb = 1 - epa;
+  var gpa = glickoRatings[team1]._E(glickoRatings[team2].__rating, glickoRatings[team2].__rd);
+  var gpb = 1 - gpa;
+
+  var outcomes = [];
+  var tally = {};
+  tally[team1] = [0, 0];
+  tally[team2] = [0, 0];
+
+  for (var i = 0; i < n; i++) {
+    var base = binomial(n + i - 1, i) * Math.pow(epa, n) * Math.pow(epb, i);
+    outcomes.push([base, n, i, team1, team2, n, i]);
+    tally[team1][1] += base;
+    tally[team2][0] += base;
+  }
+
+  for (var i = 0; i < n; i++) {
+    var base = binomial(n + i - 1, i) * Math.pow(epb, n) * Math.pow(epa, i);
+    outcomes.push([base, i, n, team2, team1, n, i]);
+    tally[team2][1] += base;
+    tally[team1][0] += base;
+  }
+
+  outcomes = sortBy(outcomes, function(o) {
+    return o[1] - o[2];
+  });
+
+  var probs = map(outcomes, function(o) {
+    return o[0];
+  });
+
+  function sup(i) {
+    return -Math.min(sum(probs.slice(0, i + 1)), sum(probs.slice(i)));
+  }
+
+  var ind = 0;
+  var objective = sup(0);
+
+  for (var i = 1; i < probs.length; i++) {
+    var p = sup(i);
+    if (p < objective) {
+      objective = p;
+      ind = i;
+    }
+  }
+
   return {
     'elo': elo.expectedScore(eloRatings[team1], eloRatings[team2]),
-    'glicko': glickoRatings[team1]._E(glickoRatings[team2].__rating, glickoRatings[team2].__rd)
+    'glicko': glickoRatings[team1]._E(glickoRatings[team2].__rating, glickoRatings[team2].__rd),
+    'outcomes': outcomes,
+    'tally': tally,
+    'median': outcomes[ind]
   };
 }
 
